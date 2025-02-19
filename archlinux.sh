28d0a8f4d50cf837eec3d4692f4c08fba827aa3c
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2025-02-19 19:54:39 +0800

    Script improvement

--- archlinux.sh
+++ archlinux.sh
@@ -5,6 +5,7 @@ echo "==        Welcome To The Arch Linux Installation Script        =="
 echo "================================================================="
 
 timedatectl set-ntp true
+reflector --sort rate --latest 6 --protocol https --save /etc/pacman.d/mirrorlist
 pacman-key --init --noconfirm --needed
 
 echo "================================================================="
