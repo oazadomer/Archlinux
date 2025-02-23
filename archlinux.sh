31da75faebd3c7ef7f7a83f75a5e6c935833ee0c
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2025-02-23 09:48:18 +0800

    Added usbutils

--- archlinux.sh
+++ archlinux.sh
@@ -151,9 +151,9 @@ echo "==                    INSTALLING Arch Linux                    =="
 echo "================================================================="
 
 if [[ $KERNEL == "1" ]] then
-    pacstrap -K /mnt base base-devel linux linux-firmware linux-headers vim grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl reflector rsync networkmanager wpa_supplicant usb_modeswitch nss-mdns modemmanager iwd ethtool dnsutils dnsmasq dhclient wireless-regdb wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion bash-language-server pacman-contrib pkgfile rebuild-detector mousetweaks
+    pacstrap -K /mnt base base-devel linux linux-firmware linux-headers vim grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl reflector rsync networkmanager wpa_supplicant usb_modeswitch nss-mdns modemmanager iwd ethtool dnsutils dnsmasq dhclient wireless-regdb wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion pacman-contrib pkgfile rebuild-detector mousetweaks usbutils
 else
-    pacstrap -K /mnt base base-devel linux-lts linux-firmware linux-lts-headers vim grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl reflector rsync networkmanager wpa_supplicant usb_modeswitch nss-mdns modemmanager iwd ethtool dnsutils dnsmasq dhclient wireless-regdb wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion bash-language-server pacman-contrib pkgfile rebuild-detector mousetweaks
+    pacstrap -K /mnt base base-devel linux-lts linux-firmware linux-lts-headers vim grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl reflector rsync networkmanager wpa_supplicant usb_modeswitch nss-mdns modemmanager iwd ethtool dnsutils dnsmasq dhclient wireless-regdb wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion pacman-contrib pkgfile rebuild-detector mousetweaks usbutils
 fi
 
 genfstab -U /mnt >> /mnt/etc/fstab
