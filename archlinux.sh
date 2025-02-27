6bbd428395b20a3f6af70cec57c1afd7738ffd0a
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2025-02-28 00:56:05 +0800

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -211,7 +211,7 @@ sed -i 's/^#ParallelDownloads/ParallelDownloads = 3/' /etc/pacman.conf
 echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" >> /etc/pacman.conf
 
-pacman -Syu; pacman pamac --noconfirm
+pacman -Syu; pacman -S pamac --noconfirm
 
 sed -i "s/^#EnableAUR/EnableAUR/" /etc/pamac.conf
 pamac update all --no-confirm
