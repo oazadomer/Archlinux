ceb4a722a9fe24017ea355e037e6f8f180ae83fb
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2025-03-06 16:35:01 +0800

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -148,9 +148,9 @@ echo "==                    INSTALLING Arch Linux                    =="
 echo "================================================================="
 
 if [[ $KERNEL == "1" ]] then
-    pacstrap -K /mnt base base-devel linux linux-firmware linux-headers zsh neovim grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl libaio reflector rsync networkmanager usb_modeswitch wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion pacman-contrib pkgfile rebuild-detector mousetweaks usbutils zram-generator                                          
+    pacstrap -K /mnt base base-devel linux linux-firmware linux-headers zsh neovim-qt grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl libaio reflector rsync networkmanager usb_modeswitch wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion pacman-contrib pkgfile rebuild-detector mousetweaks usbutils zram-generator                                          
 else
-    pacstrap -K /mnt base base-devel linux-lts linux-firmware linux-lts-headers zsh neovim grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl libaio reflector rsync networkmanager usb_modeswitch wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion pacman-contrib pkgfile rebuild-detector mousetweaks usbutils zram-generator                                                  
+    pacstrap -K /mnt base base-devel linux-lts linux-firmware linux-lts-headers zsh neovim-qt grub efibootmgr inotify-tools git python rust gcc make cmake less wget curl libaio reflector rsync networkmanager usb_modeswitch wireless_tools smartmontools mtools net-tools dosfstools efitools nfs-utils nilfs-utils exfatprogs ntfs-3g ntp openssh cronie bash-completion pacman-contrib pkgfile rebuild-detector mousetweaks usbutils zram-generator                                                  
 fi
 
 genfstab -U /mnt >> /mnt/etc/fstab
