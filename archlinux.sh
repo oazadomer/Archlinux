c81a1ca8ab7e245357dc0d0e3f6acc6054c4e437
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2025-04-22 21:06:32 +0800

    Script Improvement

--- archlinux.sh
+++ archlinux.sh
@@ -223,6 +223,11 @@ console-mode max
 editor no
 EOF
 
+cat <<EOF > /boot/loader/entries/windows.conf
+ title    Windows Boot Manager
+ linux    /EFI/Microsoft/Boot/bootmgfw.efi
+ EOF
+
     if [[ $KERNEL == "1" ]]; then
 cat <<EOF > /boot/loader/entries/arch.conf
 title    Arch Linux
