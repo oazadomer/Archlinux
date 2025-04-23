a853770cba596dadf1681662557c8451d9575553
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2025-04-23 14:59:00 +0800

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -259,8 +259,8 @@ echo "================================================================="
 
  pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
  pacman-key --lsign-key 3056513887B78AEB
- pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
- pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+ pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm --needed
+ pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm --needed
  
  sed -i 's/^#Color/Color/' /etc/pacman.conf
  sed -i '/Color/a ILoveCandy' /etc/pacman.conf
