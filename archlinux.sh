1f6e88adb0aa672b51daf2ea75727b3cd900afcc
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2026-08-18 14:32:40 +0800

    Update package installation for desktop 3
    Removed 'hyprcursor' from the package installation command for desktop 3.

--- archlinux.sh
+++ archlinux.sh
@@ -272,7 +272,7 @@ elif [[ $DESKTOP == "2" ]]; then
 
 elif [[ $DESKTOP == "3" ]]; then
       retry_command pacman -S wayland wayland-utils wayland-protocols qt6-wayland qt5-wayland glfw-wayland xorg-xwayland xorg-xlsclients libxkbcommon --noconfirm --needed
-      retry_command pacman -S hyprland hyprpolkitagent hypridle hyprcursor waybar swaync swaybg swaylock-effects wlogout wireplumber udisks2 nwg-look qt5ct grim slurp swappy wl-clipboard cliphist wofi xdg-desktop-portal-hyprland xdg-desktop-portal-gtk power-profiles-daemon blueman gnome-keyring kitty kitty-shell-integration kitty-terminfo nautilus sushi pavucontrol file-roller yazi starship btop gdm f2fs-tools gufw traceroute --noconfirm --needed
+      retry_command pacman -S hyprland hyprpolkitagent hypridle waybar swaync swaybg swaylock-effects wlogout wireplumber udisks2 nwg-look qt5ct grim slurp swappy wl-clipboard cliphist wofi xdg-desktop-portal-hyprland xdg-desktop-portal-gtk power-profiles-daemon blueman gnome-keyring kitty kitty-shell-integration kitty-terminfo nautilus sushi pavucontrol file-roller yazi starship btop gdm f2fs-tools gufw traceroute --noconfirm --needed
       retry_command pacman -S vscodium catppuccin-cursors-mocha brave-origin-bin snapshot gnome-calendar gnome-contacts transmission-gtk qalculate-gtk nodejs-lts npm yarn ripgrep python-pip pyenv android-tools vala tk 7zip xz unrar unzip lzop gdb mtpfs dpkg xclip python-xlib adwaita-qt6 adwaita-qt5 yay gvfs-afc gvfs-goa gvfs-google gvfs-mtp gvfs-gphoto2 gvfs-nfs xsane gparted imv --noconfirm --needed
 #      retry_command pamac install hyprmod hyprpicker --noconfirm --needed
       
