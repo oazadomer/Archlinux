2b7501c8974b7c4ae78e3c0e03b0c9072b50286b
Azad Omer <110857012+oazadomer@users.noreply.github.com>
2026-09-01 14:33:33 +0800

    Replace 'python-pywayland' with 'picom' in package list

--- archlinux.sh
+++ archlinux.sh
@@ -312,7 +312,7 @@ elif [[ $DESKTOP == "3" ]]; then
       systemctl enable sddm
 
 elif [[ $DESKTOP == "4" ]]; then
-      retry_command pacman -S qtile python-pywayland  polkit-gnome swayidle swaync swww waypaper swaylock rofi udisks2 nwg-look qt5ct grim slurp swappy wl-clipboard cliphist xdg-desktop-portal-wlr power-profiles-daemon blueman gnome-keyring kitty kitty-shell-integration kitty-terminfo nautilus sushi file-roller sddm simple-scan loupe snapshot gnome-calendar gnome-calculator transmission-gtk --noconfirm --needed
+      retry_command pacman -S qtile python-pywayland picom polkit-gnome swayidle swaync swww waypaper swaylock rofi udisks2 nwg-look qt5ct grim slurp swappy wl-clipboard cliphist xdg-desktop-portal-wlr power-profiles-daemon blueman gnome-keyring kitty kitty-shell-integration kitty-terminfo nautilus sushi file-roller sddm simple-scan loupe snapshot gnome-calendar gnome-calculator transmission-gtk --noconfirm --needed
       retry_command pacman -S qt6-wayland qt5-wayland adwaita-qt6 adwaita-qt5 --noconfirm --needed
       
       echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/99-temp-aur-install
